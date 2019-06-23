1bad6a7f4eea5dbee3342d5f99478256cd258d3c
Kangatang <51993117+Kangatang@users.noreply.github.com>
2019-06-23 11:57:44 +1000

    Basic logic flow
    -Beginning to process the provided JSON file for the base structure of the system
    -Barebones evaluation of if the order will be successful, does not alter quantities
    -Returns base output in the expected final form

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,36 @@
-from flask import request, jsonify
+from flask import request
 from app import app
+import json
 
+# Load up the JSON values from a file
+with open('data.json', 'r') as lFile:
+    gLoadedFile = json.load(lFile)
+    # Storing the products and orders as a dictionary makes lookup more efficient
+    # than iterating through a list looking for the right product/order id, it adds 
+    # more initial setup time but should allow for faster post responses
+    gProductDict = { i["productId"] : i for i in gLoadedFile["products"] }
+    gOrderDict = { i["orderId"] : i for i in gLoadedFile["orders"] }
 
 @app.route('/api/v1/warehouse/fulfilment', methods=['POST'])
 def ProcessOrders():
-    return "Oh boy it worked"
+    content = request.json
+    #test output
+    print (content)
+    ResultDict = {"unfulfillable":[]}
+    
+    for lCurrentOrderId in content["orderIds"]:
+        #Obtain the order info for the requested order
+        lTargetOrder = gOrderDict[lCurrentOrderId]
+        lOrderPossible = True
+        for lCurrentOrderedProduct in lTargetOrder["items"]:
+            lTargetProduct = gProductDict[lCurrentOrderedProduct["productId"]]
+            if ( lTargetProduct["quantityOnHand"] < lCurrentOrderedProduct["quantity"] ):
+                #This is bad, we don't have enough stock on hand
+                lOrderPossible = False
+        
+        if lOrderPossible == False:
+            ResultDict["unfulfillable"].append(lCurrentOrderId)
+    return str(ResultDict)
     
 
 @app.errorhandler(405)
