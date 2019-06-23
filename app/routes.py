f98aa082a9759235f8c0a7181cc49ddfbab1d6c7
Kangatang <51993117+Kangatang@users.noreply.github.com>
2019-06-23 13:26:22 +1000

    Requirements logic implemented
    Implemented the basic logic of the system as per the requirements, where the incoming orders are evaluated based on currently held quantities and have their status values modified accordingly. Products that have their quantifies go below the preset amount will then have a stub order be added to a dictionary only once.

--- app/routes.py
+++ app/routes.py
@@ -10,6 +10,11 @@ with open('data.json', 'r') as lFile:
     # more initial setup time but should allow for faster post responses
     gProductDict = { i["productId"] : i for i in gLoadedFile["products"] }
     gOrderDict = { i["orderId"] : i for i in gLoadedFile["orders"] }
+    
+#If we run out of a product, we have to send off for some replacements, this is a stub
+# for our outgoing product requests
+gPendingInternalOrders = {}
+gCurrentInternalOrderId = 0
 
 @app.route('/api/v1/warehouse/fulfilment', methods=['POST'])
 def ProcessOrders():
@@ -21,6 +26,10 @@ def ProcessOrders():
     for lCurrentOrderId in content["orderIds"]:
         #Obtain the order info for the requested order
         lTargetOrder = gOrderDict[lCurrentOrderId]
+        
+        # We need to check if the order is possible first, because we don't
+        # partially fill orders, we don't want to start subtracting quantities before
+        # we know we have to, otherwise we'd have to deal with some ugly quantity rollbacks
         lOrderPossible = True
         for lCurrentOrderedProduct in lTargetOrder["items"]:
             lTargetProduct = gProductDict[lCurrentOrderedProduct["productId"]]
@@ -30,6 +39,28 @@ def ProcessOrders():
         
         if lOrderPossible == False:
             ResultDict["unfulfillable"].append(lCurrentOrderId)
+            lTargetOrder["status"] = "Error: Unfulfillable"
+        else:
+            #if the order is valid, iterate through again removing the necessary quantities
+            #we will investigatge later to see if it is necessary to submit a refill order for the products
+            for lCurrentOrderedProduct in lTargetOrder["items"]:
+                lTargetProduct = gProductDict[lCurrentOrderedProduct["productId"]]
+                lTargetProduct["quantityOnHand"] -= lCurrentOrderedProduct["quantity"]
+            lTargetOrder["status"] = "Fulfilled"
+                
+        
+        
+        
+    #Process any new orders for products below the threshold
+    #Check if order has already been sent to refill that product
+    for lCurrentProduct in gProductDict.values():
+        if lCurrentProduct["quantityOnHand"] < lCurrentProduct["reorderThreshold"]:
+            #We need more of this item, check if we have already asked before
+            #If we don't have one, make one
+            if lCurrentProduct["productId"] in gPendingInternalOrders:
+                gCurrentInternalOrderId += 1
+                gPendingInternalOrders[lCurrentProduct["productId"]] = { "orderId" : gCurrentInternalOrderId, "orderQuantity" : lCurrentProduct["reorderAmount"] }
+    
     return str(ResultDict)
     
 
